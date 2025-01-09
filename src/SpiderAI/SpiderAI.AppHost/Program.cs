c8ba5bb22bbe4e6a4fd7c7c6daa47db1855abbbf
Peter Bons <Expecho@users.noreply.github.com>
2025-01-09 20:54:29 +0100

    Remove angular client

--- src/SpiderAI/SpiderAI.AppHost/Program.cs
+++ src/SpiderAI/SpiderAI.AppHost/Program.cs
@@ -2,10 +2,4 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var backendApi = builder.AddProject<Projects.SpiderAI_Server>("spiderai-server");
 
-builder
-    .AddNpmApp("spiderai-client", "../SpiderAI.Client")
-    .WithReference(backendApi)
-    .WithHttpEndpoint(targetPort: 56779)
-    .WaitFor(backendApi);
-
 builder.Build().Run();
